4e369c47f2fbb187b4366d587f589b2b00689428
lianxiaolei <lianxiaolei@xueleyun.com>
2019-07-26 11:00:39 +0800

    update: Reconstruct codes.

--- nlp_base/info_extra/bilstm_sequence_labelling.py
+++ nlp_base/info_extra/bilstm_sequence_labelling.py
@@ -10,7 +10,17 @@ MAX_SEQ_LEN = 64
 t2i_dict = {'c': 1, 'o': 2, 'b': 3, 'a': 4}
 
 
-def word2ind_with_w2v(fname, word_set):
+def word2ind(fname, word_set):
+  """
+  Convert word to index.
+  Args:
+    fname: Source file name.
+    word_set:  A fixed word set.
+
+  Returns:
+    sentences: A 2D list with word index.
+    tags:      A 2D list with word tags.
+  """
   sentences = []
   tags = []
   with open(fname, 'r', encoding='utf8') as fin:
@@ -38,7 +48,17 @@ def word2ind_with_w2v(fname, word_set):
   return sentences, tags
 
 
-def get_t2i_map(fname, preserve_zero=True):
+def get_w2i_map(fname, preserve_zero=True):
+  """
+  Generate word set.
+  Args:
+    fname:         Source file name.
+    preserve_zero: Preserve the place for '<UNK>'.
+
+  Returns:
+    word_set:      A set contains word index.
+
+  """
   word_list = []
 
   with open(fname, 'r', encoding='utf8') as fin:
@@ -61,14 +81,14 @@ def get_t2i_map(fname, preserve_zero=True):
 
 def data_iterate(X, y, batch_size):
   """
-
+  Generate iterator for train dataset, dev dataset and test dataset.
   Args:
-    X:
-    y:
-    batch_size:
+    X:          Features
+    y:          Labels
+    batch_size: Batch size.
 
   Returns:
-
+    Return three initializer and a iterator.
   """
   # Split dataset to train and test.
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
@@ -94,6 +114,9 @@ def data_iterate(X, y, batch_size):
 
 
 class SequenceLabelling(object):
+  """
+  Sequence labelling model class.
+  """
   def __init__(self, num_classes, vocab_length, word_dim, units, rnn_layer_num, keep_prob):
     self.num_classes = num_classes
     self.vocab_length = vocab_length
@@ -102,7 +125,7 @@ class SequenceLabelling(object):
     self.rnn_layer_num = rnn_layer_num
     self.keep_prob = keep_prob
 
-  def lstm_cell(self, reuse=False):
+  def _lstm_cell(self, reuse=False):
     if reuse:
       tf.get_variable_scope.reuse_variables()
     cell = rnn.LSTMCell(self.units)
@@ -111,17 +134,18 @@ class SequenceLabelling(object):
   def _init_embedding(self):
     self.embedding = tf.Variable(tf.random_normal([self.vocab_length, self.word_dim]), dtype=tf.float32)
 
-  def lookup(self, X):
-    self.inputs = tf.nn.embedding_lookup(self.embedding, X + 1)
+  def _lookup(self, X):
+    lookup = tf.nn.embedding_lookup(self.embedding, X + 1)
+    return lookup
 
-  def rnn_units(self):
+  def _rnn_units(self, X):
     # Defind rnn base units.
-    cell_fw = [self.lstm_cell() for _ in range(self.rnn_layer_num)]
-    cell_bw = [self.lstm_cell() for _ in range(self.rnn_layer_num)]
+    cell_fw = [self._lstm_cell() for _ in range(self.rnn_layer_num)]
+    cell_bw = [self._lstm_cell() for _ in range(self.rnn_layer_num)]
 
     # Unstack the inputs to list with step items.
     # TODO Why we need to unstack the timestep?
-    inputs = tf.unstack(self.inputs, 64, axis=1)
+    inputs = tf.unstack(X, 64, axis=1)
 
     output, _, _ = tf.contrib.rnn.stack_bidirectional_rnn(cell_fw, cell_bw,
                                                           inputs=inputs, dtype=tf.float32)
@@ -132,23 +156,63 @@ class SequenceLabelling(object):
     # Reshape output to [batch * times, units * 2]
     return tf.reshape(output, [-1, self.units * 2])
 
-  def compile(self, X):
+  def _tail(self, X):
     logits = tf.keras.layers.Dense(self.num_classes)(X)
-    y_predict = tf.cast(tf.argmax(logits, axis=1), tf.int32)
+    return logits
 
-    y_label_reshape = tf.cast(tf.reshape(self.y, [-1]), tf.int32)
+  def _compile(self, logits, labels):
+    logits = tf.cast(tf.argmax(logits, axis=1), tf.int32)
+
+    label_reshape = tf.cast(tf.reshape(labels, [-1]), tf.int32)
     # Prediction
-    correct_prediction = tf.equal(y_predict, y_label_reshape)
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    correct_prediction = tf.equal(logits, label_reshape)
+    self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     # Loss
-    cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-      labels=y_label_reshape, logits=tf.cast(logits, tf.float32)))
+    self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+      labels=label_reshape, logits=tf.cast(logits, tf.float32)))
 
+  def build(self, X, y):
+    self._init_embedding()
+    lookup = self._lookup(X)
+    rnn_outputs = self._rnn_units(lookup)
+    logits = self._tail(rnn_outputs)
+    self._compile(logits, y)
+
+  def operate(self, lr=1e-3):
     # Train
-    train = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+    self.train_op = tf.train.AdamOptimizer(lr).minimize(self.loss)
+    # Defind model saver.
+    self.saver = tf.train.Saver()
 
-    saver = tf.train.Saver()
+  def run_epoch(self, train_initializer, dev_initializer=None, lr=1e-3,
+                epoch_num=100, step_num=200, dev_step=10, save_when_acc=0.94):
+    self.operate(lr=lr)
 
+    with tf.Session() as sess:
+      sess.run(tf.initialize_all_variables())
+      for epoch in range(epoch_num):
+        # tf.train.global_step(sess, global_step_tensor=global_step)
+
+        # Train
+        sess.run(train_initializer)
+        bar = tqdm(range(step_num), ncols=100)
+        for step in bar:
+          loss, acc, _ = sess.run([self.loss, self.accuracy, self.train_op])
+
+          bar.set_description_str("Step:{}\t  Loss:{}\t  Acc:{}".format(step, str(loss)[:5], str(acc)[:5]))
+
+        # Dev
+        sess.run(dev_initializer)
+        bar = tqdm(range(dev_step), ncols=100)
+        for step in bar:
+          acc = sess.run(self.accuracy)
+
+          bar.set_description_str("Step:{}\tAcc:{}".format(step, acc))
+
+        if acc > save_when_acc:
+          self.saver.save(sess, '/home/lian/PycharmProjects/NLP-base/model/checkpoint/seqtag_ckpt',
+                          global_step=step)
+          print('Saved model done.')
 
 
 if __name__ == '__main__':
@@ -156,68 +220,3 @@ if __name__ == '__main__':
   sentences, tags = word2ind_with_w2v('/home/lian/data/nlp/datagrand_info_extra/train.txt', word_set)
   sentences = np.array(sentences)
   tags = np.array(tags)
-
-  train_initializer, dev_initializer, test_initializer, iterator = data_iterate(sentences, tags, 10)
-
-  x, y = iterator.get_next()
-
-  embedding = tf.Variable(tf.random_normal([4550, 20]), dtype=tf.float32)
-  inputs = tf.nn.embedding_lookup(embedding, x + 1)
-
-  cell_fw = [lstm_cell(128, 1) for _ in range(1)]
-  cell_bw = [lstm_cell(128, 1) for _ in range(1)]
-  inputs = tf.unstack(inputs, 64, axis=1)
-  output, _, _ = tf.contrib.rnn.stack_bidirectional_rnn(cell_fw, cell_bw,
-                                                        inputs=inputs, dtype=tf.float32)
-
-  output = tf.stack(output, axis=1)
-  print(output.shape)
-  output = tf.reshape(output, [-1, 128 * 2])
-
-  logits = tf.keras.layers.Dense(5)(output)
-  y_predict = tf.cast(tf.argmax(logits, axis=1), tf.int32)
-  print('Output Y', y_predict)
-
-  # Reshape y_label
-  y_label_reshape = tf.cast(tf.reshape(y, [-1]), tf.int32)
-  # Prediction
-  correct_prediction = tf.equal(y_predict, y_label_reshape)
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  # Loss
-  cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-    labels=y_label_reshape, logits=tf.cast(logits, tf.float32)))
-
-  # Train
-  train = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
-
-  saver = tf.train.Saver()
-
-  acc_thres = 0.94
-  with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
-    for epoch in range(100):
-      # tf.train.global_step(sess, global_step_tensor=global_step)
-
-      # Train
-      sess.run(train_initializer)
-      bar = tqdm(range(200), ncols=100)
-      for step in bar:
-        loss, acc, _ = sess.run([cross_entropy, accuracy, train])
-
-        bar.set_description_str("Step:{}\t  Loss:{}\t  Acc:{}".format(step, str(loss)[:5], str(acc)[:5]))
-        # if step % 10 == 0:
-        #   print('Step', step, 'Train Loss', loss, 'Accuracy', acc)
-
-      # Dev
-      sess.run(dev_initializer)
-      bar = tqdm(range(10), ncols=10)
-      for step in bar:
-        acc = sess.run(accuracy)
-
-        bar.set_description_str("Step:{}\tAcc:{}".format(step, acc))
-        # if step % 20 == 0:
-        #   print('Dev Accuracy', acc)
-
-      if acc > acc_thres:
-        saver.save(sess, '/home/lian/PycharmProjects/NLP-base/model/checkpoint/seqtag_ckpt', global_step=step)
-        print('Saved model done.')
--- test.py
+++ test.py
@@ -1,6 +1,72 @@
-import yaml
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import numpy as np
+from tensorflow.contrib import rnn
+from tqdm import tqdm
 
-with open('conf/settings.yml', 'r', encoding='utf8') as fin:
-  conf = yaml.load(fin, Loader=yaml.FullLoader)
 
-print(conf)
+if __name__ == '__main__':
+  train_initializer, dev_initializer, test_initializer, iterator = data_iterate(sentences, tags, 10)
+
+  x, y = iterator.get_next()
+
+  embedding = tf.Variable(tf.random_normal([4550, 20]), dtype=tf.float32)
+  inputs = tf.nn.embedding_lookup(embedding, x + 1)
+
+  cell_fw = [lstm_cell(128, 1) for _ in range(1)]
+  cell_bw = [lstm_cell(128, 1) for _ in range(1)]
+  inputs = tf.unstack(inputs, 64, axis=1)
+  output, _, _ = tf.contrib.rnn.stack_bidirectional_rnn(cell_fw, cell_bw,
+                                                        inputs=inputs, dtype=tf.float32)
+
+  output = tf.stack(output, axis=1)
+  print(output.shape)
+  output = tf.reshape(output, [-1, 128 * 2])
+
+  logits = tf.keras.layers.Dense(5)(output)
+  y_predict = tf.cast(tf.argmax(logits, axis=1), tf.int32)
+  print('Output Y', y_predict)
+
+  # Reshape y_label
+  y_label_reshape = tf.cast(tf.reshape(y, [-1]), tf.int32)
+  # Prediction
+  correct_prediction = tf.equal(y_predict, y_label_reshape)
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  # Loss
+  cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+    labels=y_label_reshape, logits=tf.cast(logits, tf.float32)))
+
+  # Train
+  train = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+
+  saver = tf.train.Saver()
+
+  acc_thres = 0.94
+  with tf.Session() as sess:
+    sess.run(tf.initialize_all_variables())
+    for epoch in range(100):
+      # tf.train.global_step(sess, global_step_tensor=global_step)
+
+      # Train
+      sess.run(train_initializer)
+      bar = tqdm(range(200), ncols=100)
+      for step in bar:
+        loss, acc, _ = sess.run([cross_entropy, accuracy, train])
+
+        bar.set_description_str("Step:{}\t  Loss:{}\t  Acc:{}".format(step, str(loss)[:5], str(acc)[:5]))
+        # if step % 10 == 0:
+        #   print('Step', step, 'Train Loss', loss, 'Accuracy', acc)
+
+      # Dev
+      sess.run(dev_initializer)
+      bar = tqdm(range(10), ncols=10)
+      for step in bar:
+        acc = sess.run(accuracy)
+
+        bar.set_description_str("Step:{}\tAcc:{}".format(step, acc))
+        # if step % 20 == 0:
+        #   print('Dev Accuracy', acc)
+
+      if acc > acc_thres:
+        saver.save(sess, '/home/lian/PycharmProjects/NLP-base/model/checkpoint/seqtag_ckpt', global_step=step)
+        print('Saved model done.')
