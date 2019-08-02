ff7e603daaf091f02d6aed8c81dc030a76aa399b
lianxiaolei <lianxiaolei@xueleyun.com>
2019-08-02 20:29:19 +0800

    fixbug: modify sequence length calculation.

--- nlp_base/info_extra/bilstm_sequence_labelling.py
+++ nlp_base/info_extra/bilstm_sequence_labelling.py
@@ -4,9 +4,10 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.contrib import rnn
 from tqdm import tqdm
-from nlp_base.info_extra import data_iterate, word2ind, get_w2i_map, word2ind_without_seg, test_data_iterate
+from nlp_base.info_extra import data_iterate, word2ind, word2ind_with_seqlen, get_w2i_map, \
+  word2ind_without_seg, test_data_iterate, data_iterate_with_seqlen
 
-MAX_SEQ_LEN = 32
+MAX_SEQ_LEN = 256
 t2i_dict = {'c': 1, 'o': 2, 'b': 3, 'a': 4}
 CHECKPOINT = '/home/lian/PycharmProjects/NLP-base/model/checkpoint/seqtag_ckpt'
 
@@ -23,6 +24,7 @@ class SequenceLabelling(object):
     self.units = units
     self.rnn_layer_num = rnn_layer_num
     self.keep_prob = keep_prob
+    self.sess = tf.Session()
 
   def _lstm_cell(self, reuse=False):
     if reuse:
@@ -53,7 +55,9 @@ class SequenceLabelling(object):
     output = tf.stack(output, axis=1)
 
     # Reshape output to [batch * times, units * 2]
-    return tf.reshape(output, [-1, self.units * 2])
+    # modify01
+    # return tf.reshape(output, [-1, self.units * 2])
+    return output
 
   def _tail(self, X):
     logits = tf.keras.layers.Dense(self.num_classes)(X)
@@ -61,13 +65,21 @@ class SequenceLabelling(object):
 
   def _compile(self, logits, labels):
     # Reshape
-    label = tf.cast(tf.reshape(labels, [-1]), tf.int32)
-    # Loss
-    J = tf.nn.sparse_softmax_cross_entropy_with_logits(
-      labels=label, logits=tf.cast(logits, tf.float32))
-    self.loss = J
+    # modify01
+    # labels = tf.cast(tf.reshape(labels, [-1]), tf.int32)
+    labels = tf.cast(labels, tf.int32)
 
-  def build(self, X, y, mode='train'):
+    # Loss
+    # J = tf.nn.sparse_softmax_cross_entropy_with_logits(
+    #   labels=label, logits=tf.cast(logits, tf.float32))
+    # self.loss = J
+
+    log_likelihood, transition_params = tf.contrib.crf.crf_log_likelihood(inputs=logits, tag_indices=labels,
+                                                                          sequence_lengths=self.seq_len)
+    self.loss = tf.reduce_mean(-log_likelihood)
+
+  def build(self, X, y, seq_len, mode='train'):
+    self.seq_len = seq_len
     self._init_embedding()
     lookup = self._lookup(X)
     rnn_outputs = self._rnn_units(lookup)
@@ -76,16 +88,22 @@ class SequenceLabelling(object):
     self._compile(logits, y)
 
     # Prediction
-    self.pred = tf.cast(tf.argmax(logits, axis=1), tf.int64)
-    correct_prediction = tf.equal(self.pred, tf.reshape(y, [-1]))
-    print('pred shape {}, label shape {}'.format(self.pred.shape, y.shape))
+    # modify01
+    # self.pred = tf.cast(tf.argmax(logits, axis=1), tf.int64)
+    self.pred = tf.cast(tf.argmax(logits, axis=2), tf.int64)
+    # modify01
+    # correct_prediction = tf.equal(self.pred, tf.reshape(y, [-1]))
+    correct_prediction = tf.equal(self.pred, y)
+    # print('pred shape {}, label shape {}'.format(self.pred.shape, y.shape))
     self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     if mode == 'test':
       self.X = X
       self.y = y
+      self.restore()
 
   def operate(self, lr=1e-3):
+
     # Train
     self.train_op = tf.train.AdamOptimizer(lr).minimize(self.loss)
     # Defind model saver.
@@ -93,12 +111,12 @@ class SequenceLabelling(object):
 
   def restore(self):
     saver = tf.train.Saver()
-    with tf.Session() as sess:
-      self.model = saver.restore(sess, tf.train.latest_checkpoint(CHECKPOINT[:CHECKPOINT.rfind('/')]))
+    with self.sess.as_default() as sess:
+      saver.restore(sess, tf.train.latest_checkpoint(CHECKPOINT[:CHECKPOINT.rfind('/')]))
     print('Load model successfully.')
 
   def inference(self, test_initializer, word_set):
-    with tf.Session() as sess:
+    with self.sess.as_default() as sess:
       sess.run(tf.initialize_all_variables())
       sess.run(train_initializer)
       sess.run(test_initializer)
@@ -119,12 +137,14 @@ class SequenceLabelling(object):
                 epoch_num=100, step_num=1000, dev_step=10, save_when_acc=0.94, mode='train'):
     self.operate(lr=lr)
 
-    with tf.Session() as sess:
+    with self.sess.as_default() as sess:
       sess.run(tf.initialize_all_variables())
       if mode == 'test':
         sess.run(train_initializer)
-        acc = sess.run(self.accuracy)
+        X, pred, acc = sess.run([self.X, self.pred, self.accuracy])
         print('accuracy', acc)
+        print('X', X)
+        print('pred', pred)
         sys.exit(0)
       for epoch in range(epoch_num):
         # tf.train.global_step(sess, global_step_tensor=global_step)
@@ -144,13 +164,14 @@ class SequenceLabelling(object):
         sess.run(dev_initializer)
         bar = tqdm(range(dev_step), ncols=100)
         for step in bar:
-          acc = sess.run(self.accuracy)
-
+          try:
+            acc = sess.run(self.accuracy)
+          except tf.errors.OutOfRangeError:
+            sess.run(dev_initializer)
           bar.set_description_str("Step:{}\tAcc:{}".format(step, acc))
 
         if acc > save_when_acc:
-          self.saver.save(sess, CHECKPOINT,
-                          global_step=epoch)
+          self.saver.save(sess, CHECKPOINT, global_step=epoch)
           print('Saved model done.')
 
 
@@ -159,31 +180,31 @@ if __name__ == '__main__':
 
   word_set = get_w2i_map('/home/lian/data/nlp/datagrand_info_extra/train.txt')
   print('word set', word_set)
-  sentences, tags = word2ind('/home/lian/data/nlp/datagrand_info_extra/train.txt', word_set)
+  # sentences, tags = word2ind('/home/lian/data/nlp/datagrand_info_extra/train.txt', word_set)
+  sentences, tags, seq_lens = word2ind_with_seqlen('/home/lian/data/nlp/datagrand_info_extra/train.txt', word_set)
   sentences = np.array(sentences)
   tags = np.array(tags)
-  print('train sentences\n', [word_set[item] for line in sentences for item in line][:10])
-  print('train sentences index\n', sentences[:10])
+  seq_lens = np.array(seq_lens)
+  print('Batch sequence length', seq_lens)
+
+  # print('train sentences\n', [word_set[item] for line in sentences for item in line][:10])
+  # print('train sentences index\n', sentences[:10])
   test_sentences = word2ind_without_seg('/home/lian/data/nlp/datagrand_info_extra/test.txt', word_set)
-  print('test sentences\n', [word_set[item] for line in test_sentences for item in line][:10])
-  print('test sentences index\n', test_sentences[:10])
+  # print('test sentences\n', [word_set[item] for line in test_sentences for item in line][:10])
+  # print('test sentences index\n', test_sentences[:10])
 
-  # sys.exit(0)
-
-  train_initializer, dev_initializer, _, iterator = data_iterate(sentences, tags, 100)
+  train_initializer, dev_initializer, _, iterator = data_iterate_with_seqlen(sentences, tags, seq_lens, 200)
   test_initializer, test_iterator = test_data_iterate(sentences, 100)
 
-  X, y = iterator.get_next()
+  X, y, seq_len = iterator.get_next()
   test = test_iterator.get_next()
 
-  model = SequenceLabelling(5, 4550, 128, 128, 1, 1.)
+  model = SequenceLabelling(num_classes=5, vocab_length=4550, word_dim=128, units=128, rnn_layer_num=1, keep_prob=0.88)
 
   # model.build(X, y, mode='train')
-  model.build(X, y, mode='test')
+  model.build(X, y, seq_len, mode='train')
 
-  # model.restore()
-
-  model.run_epoch(train_initializer, dev_initializer, 1e-3, mode='test')
+  model.run_epoch(train_initializer, dev_initializer, 1e-3, mode='train', save_when_acc=0.99)
   # model.inference(test_initializer, word_set)
 
   print('All done.')
